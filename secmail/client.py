d5262e1d58586774d42106adfecb5665083ca881
qualia-5w4 <geniusnaru@gmail.com>
2023-07-13 18:39:07 +0900

    add DOMAIN_LIST in config.py

--- secmail/client.py
+++ secmail/client.py
@@ -2,6 +2,15 @@ import os
 import random
 import httpx
 
+from .config import (
+    DOMAIN_LIST,
+    GEN_RANDOM_MAILBOX,
+    GET_DOMAIN_LIST,
+    GET_MESSAGES,
+    SINGLE_MESSAGE,
+    DOWNLOAD,
+)
+
 
 class Client:
     """An API wrapper for www.1secmail.com written in Python.
@@ -15,8 +24,12 @@ class Client:
         self.host = "https://" + host + "/api/v1/"
         self.client = httpx.Client()
 
-    def _request(self):
+    def _request(self, method, url, params, json, data_type):
         pass
 
-    def create_random_email(self, amount: int):
+    def get_active_domains(self):
+        pass
+
+    def generate_random_email(self, amount: int, domain: str):
+        """Generating random email addresses"""
         pass
--- secmail/config.py
+++ secmail/config.py
@@ -1,5 +1,16 @@
 VERSION = "0.0.1"
 
+DOMAIN_LIST = [
+    "1secmail.com",
+    "1secmail.org",
+    "1secmail.net",
+    "kzccv.com",
+    "qiott.com",
+    "wuuvo.com",
+    "icznn.com",
+    "ezztt.com",
+]
+
 GEN_RANDOM_MAILBOX = "?action=genRandomMailbox"
 GET_DOMAIN_LIST = "?action=getDomainList"
 GET_MESSAGES = "?action=getMessages"
