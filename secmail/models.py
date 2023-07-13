9fdaacadd3e7ef23da92292f02bdc5e496b2b19a
qualia-5w4 <geniusnaru@gmail.com>
2023-07-13 22:51:14 +0900

    add message object model

--- secmail/models.py
+++ secmail/models.py
@@ -2,6 +2,8 @@ class MailBox:
     """The mailbox object contains an array of email objects, each with an unique ID,\n
     sender's email address, subject line, and date and time the email was sent.
 
+    ---
+
     Attributes:
     ----------
 
@@ -19,7 +21,7 @@ class MailBox:
 
     def __init__(self, response) -> None:
         self.id = response.get("id")
-        self.from_address = response.get("from_address")
+        self.from_address = response.get("from")
         self.subject = response.get("subject")
         self.date = response.get("date")
 
@@ -28,7 +30,62 @@ class MailBox:
 
 
 class Message:
-    pass
+    """The message object contains an unique ID, sender's email address,\n
+    subject line, date and time the email was sent, list of attachment object,\n
+    body (html if exists, text otherwise), text body and HTML body.
+
+    ---
+
+    Attributes:
+    ----------
+
+    - id : (``int``) - Message id
+
+    - from_address : (``str``) - Sender email address
+
+    - subject : (``str``) - Subject
+
+    - date : (``str``) - Receive date
+
+    - attachments : (``str``) - List of Attachment object
+
+    - body : (``str``) - Message body (html if exists, text otherwise)
+
+    - text_body : (``str``) - Message body (text)
+
+    - html_body : (``str``) - Message body (html)
+
+    """
+
+    __slots__ = (
+        "id",
+        "from_address",
+        "subject",
+        "date",
+        "attachments",
+        "body",
+        "text_body",
+        "html_body",
+    )
+
+    def __init__(self, response) -> None:
+        self.id = response.get("id")
+        self.from_address = response.get("from")
+        self.subject = response.get("subject")
+        self.date = response.get("date")
+
+        self.attachments = response.get("attachments")
+        if self.attachments is not None:
+            self.attachments = [
+                Attachment(attachment) for attachment in self.attachments
+            ]
+
+        self.body = response.get("body")
+        self.text_body = response.get("textBody")
+        self.html_body = response.get("htmlBody")
+
+    def __str__(self) -> str:
+        return f"Message(id={self.id}, from_address={self.from_address}, subject={self.subject}, date={self.date})"
 
 
 class Attachment:
