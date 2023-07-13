60241da2611e38c40834c5f3297d03b2faf56d28
qualia-5w4 <geniusnaru@gmail.com>
2023-07-13 21:56:49 +0900

    add staticmethods

--- secmail/client.py
+++ secmail/client.py
@@ -87,6 +87,7 @@ class Client:
     """
 
     def __init__(self, base_path=current_path, host="www.1secmail.com") -> None:
+        self.base_path = base_path
         self.host = "https://" + host + "/api/v1/"
         self.client = httpx.Client()
 
@@ -120,11 +121,8 @@ class Client:
     def _split_email(address: str) -> tuple:
         return address.split("@")
 
-    def get_active_domains(self) -> list:
-        """Get list of currently active domains."""
-        return self._request(method="GET", url=f"{self.host + GET_DOMAIN_LIST}")
-
-    def random_email(self, amount: int, domain: str = None) -> list:
+    @staticmethod
+    def random_email(amount: int, domain: str = None) -> list:
         """Generate a list of random email addresses."""
         if domain is not None and domain not in DOMAIN_LIST:
             err_msg = (
@@ -143,16 +141,8 @@ class Client:
 
         return emails
 
-    def delete_email(self, address: str) -> str:
-        """Delete specific email address."""
-        username, domain = self._split_email(address)
-        return self._request(
-            method="DELETE",
-            url=f"{self.host + DELETE_MAILBOX}",
-            params={"login": username, "domain": domain},
-        )
-
-    def custom_email(self, username: str, domain: str = "1secmail.com") -> str:
+    @staticmethod
+    def custom_email(username: str, domain: str = "1secmail.com") -> str:
         """Generate custom email address."""
         if domain is not None and domain not in DOMAIN_LIST:
             err_msg = (
@@ -166,6 +156,24 @@ class Client:
             email = username + "@" + random.choice(DOMAIN_LIST)
 
         return email
+
+    @staticmethod
+    def save_email(address: str):
+        """Save email to json file."""
+        pass
+
+    def get_active_domains(self) -> list:
+        """Get list of currently active domains."""
+        return self._request(method="GET", url=f"{self.host + GET_DOMAIN_LIST}")
+
+    def delete_email(self, address: str) -> str:
+        """Delete specific email address."""
+        username, domain = self._split_email(address)
+        return self._request(
+            method="DELETE",
+            url=f"{self.host + DELETE_MAILBOX}",
+            params={"login": username, "domain": domain},
+        )
 
     def get_messages(self, address: str) -> list:
         """Check your mailbox."""
@@ -187,10 +195,6 @@ class Client:
 
     def await_new_message(self, address: str):
         """Wait until you receive a new message."""
-        pass
-
-    def save_email(self, address: str):
-        """Save email to json file."""
         pass
 
     def download_attachment(self, address: str, message_id: int, filename: str):
